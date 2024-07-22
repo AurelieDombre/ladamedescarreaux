834453e5a454bd5cdc07f6b012f8d2ac97fe7f8f
dombre <aure.dombre.formation@gmail.com>
2024-07-22 16:24:23 +0200

    Galerie preparation du grid

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,3 +18,25 @@ scroll.on("scroll", () => {
         document.body.style.color = "#000101";
     }
 });
+
+
+
+let allGridItems = [...document.getElementsByClassName("grid-item")];
+let popupBg = document.getElementById("popup-bg");
+let popupImg = document.getElementById("popup-img");
+
+const openPopup = (e) => {
+    let clickedImageSrc = e.target.src;
+    popupBg.classList.add("active");
+    popupImg.src = clickedImageSrc;
+};
+
+const closePopup = () => {
+    popupBg.classList.remove("active");
+};
+
+allGridItems.forEach((el) => el.addEventListener("click", openPopup));
+
+popupImg.addEventListener("click", (e) => e.stopPropagation());
+popupBg.addEventListener("click", closePopup);
+
