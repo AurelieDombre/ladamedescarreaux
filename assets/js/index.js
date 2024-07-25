2e604faca496873257ce55fd4f0fbecd4d953240
dombre <aure.dombre.formation@gmail.com>
2024-07-25 16:38:30 +0200

    contact

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,4 +41,28 @@ document.addEventListener("DOMContentLoaded", () => {
     allGridItems.forEach((el) => el.addEventListener("click", openPopup));
     popupImg.addEventListener("click", (e) => e.stopPropagation());
     popupBg.addEventListener("click", closePopup);
+
+    const inputs = document.querySelectorAll(".input");
+
+    function focusFunc() {
+        let parent = this.parentNode;
+        parent.classList.add("focus");
+    }
+    
+    function blurFunc() {
+        let parent = this.parentNode;
+        if (this.value == "") {
+            parent.classList.remove("focus");
+        }
+    }
+    
+    inputs.forEach((input) => {
+        input.addEventListener("focus", focusFunc);
+        input.addEventListener("blur", blurFunc);
+    });
+    
+
+
+
+
 });
